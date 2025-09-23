e9dde3ed8f354d06dc6956e5aaeb7f0d75c61f3c
Jorge Luis Bermudez Buendia <george-1002@hotmail.com>
2025-09-23 17:48:39 -0500

    corrige configuración i18n y detección de idioma en app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-sofkau';
+  currentLang: string = 'en'; // idioma por defecto
+
+  ngOnInit(): void {
+    this.detectBrowserLanguage();
+  }
+
+  detectBrowserLanguage(): void {
+    const lang = navigator.language; // ej. "es-CO", "en-US"
+    const shortLang = lang.split('-')[0]; // solo "es", "en"
+    this.currentLang = shortLang;
+
+    console.log(`Idioma del navegador detectado: ${this.currentLang}`);
+  }
 }
