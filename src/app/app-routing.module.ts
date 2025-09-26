640489c3a2ac2d6076209063f2fc9f6a56cfe59d
Alejandro Padilla Gonzalez <alejopadilla297@gmail.com>
2025-09-26 12:40:08 -0500

    fix unused code in route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
-const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: '**', redirectTo: '' },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [
